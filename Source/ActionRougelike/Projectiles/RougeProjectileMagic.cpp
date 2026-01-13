b0fb4278b2c3ce5397bc4bcf923a0eb448a37ee5
Shawn <mdzzsb233@gmail.com>
2026-01-13 16:38:00 -0700

    Add RougeProjectileMagic class and update build dependencies

---
+++ Source/ActionRougelike/Projectiles/RougeProjectileMagic.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RougeProjectileMagic.h"
+#include "RougeProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ARougeProjectileMagic::ARougeProjectileMagic()
+{
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.0f);
+	SphereComponent->SetCollisionProfileName("Projectile");
+	
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComp"));
+	ProjectileMovementComponent->InitialSpeed = 2000.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.f;
+
+}
+
---
+++ Source/ActionRougelike/Projectiles/RougeProjectileMagic.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RougeProjectileMagic.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class ACTIONROUGELIKE_API ARougeProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+public:
+	ARougeProjectileMagic();
+};
