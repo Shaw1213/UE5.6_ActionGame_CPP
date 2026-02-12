dffba1a02d02aec9292e6d279e1376d5d146b807
Shawn <mdzzsb233@gmail.com>
2026-02-12 00:12:25 -0700

    Add black hole effects and radial force
    Add Niagara visual effect and a radial force component for the black hole projectile. Initialize and attach a LoopedBlackHoleEffect and RadialForceComp, configure radius, strength, falloff, and set it to ignore pawns. Spawn the Niagara system at spawn time and start a timer to self-destruct after a configurable lifespan. Implement Tick to overlap nearby dynamic objects and destroy actors that are simulating physics, and draw a debug sphere for the force radius. Header updated with new component and property members; Blueprint uasset updated accordingly.

--- Source/ActionRougelike/Projectiles/RougeProjectileBlackhole.cpp
+++ Source/ActionRougelike/Projectiles/RougeProjectileBlackhole.cpp
@@ -3,15 +3,78 @@
 
 #include "RougeProjectileBlackhole.h"
 #include "Components/SphereComponent.h"
-#include "GameFramework/ProjectileMovementComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Engine/OverlapResult.h"
+
 
 ARougeProjectileBlackhole::ARougeProjectileBlackhole()
 {
-	
+	PrimaryActorTick.bCanEverTick = true;
+
+	LoopedBlackHoleEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedBlackHoleEffect"));
+	LoopedBlackHoleEffect->SetupAttachment(RootComponent);
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(RootComponent);
+	RadialForceComp->Radius = RadioForceRadius;
+	RadialForceComp->ForceStrength = GravityStrength;
+	RadialForceComp->Falloff = RIF_Linear;
+	RadialForceComp->bAutoActivate = true;
+	RadialForceComp->bIgnoreOwningActor = true;
+	RadialForceComp->RemoveObjectTypeToAffect (UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn)); //ignore pawns, only affect physics objects
 }
 
 void ARougeProjectileBlackhole::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
+	
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, BlackHoleEffect, GetActorLocation());
+
+	GetWorldTimerManager().SetTimer(
+		BlackHoleTimerHandle,
+		this,
+		&ARougeProjectileBlackhole::SelfDestroy,
+		BlackHoleLifespan,//5 seconds
+		false
+		);
+	
 }
 
+void ARougeProjectileBlackhole::SelfDestroy()
+{
+	Destroy();
+}
+
+void ARougeProjectileBlackhole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TArray<FOverlapResult> Overlaps;
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	QueryParams.AddIgnoredActor(GetInstigator());
+
+	bool bHasOverlaps = GetWorld()->OverlapMultiByObjectType(
+		Overlaps,
+		GetActorLocation(),
+		FQuat::Identity,
+		FCollisionObjectQueryParams::AllDynamicObjects,
+		FCollisionShape::MakeSphere(SphereComponent->GetScaledSphereRadius()),
+		QueryParams
+	);
+
+	if (bHasOverlaps)
+	{
+		for (const FOverlapResult& Overlap : Overlaps)
+		{
+			UPrimitiveComponent* OverlapComp = Overlap.GetComponent();
+			if (OverlapComp && OverlapComp->IsSimulatingPhysics())
+			{
+				Overlap.GetActor()->Destroy();
+			}
+		}
+	}
+
+	DrawDebugSphere(GetWorld(), GetActorLocation(), RadioForceRadius, 32, FColor::White);
+}
--- Source/ActionRougelike/Projectiles/RougeProjectileBlackhole.h
+++ Source/ActionRougelike/Projectiles/RougeProjectileBlackhole.h
@@ -6,16 +6,44 @@
 #include "RougeProjectile.h"
 #include "RougeProjectileBlackhole.generated.h"
 
+class URadialForceComponent;
+class UNiagaraComponent;
+class UNiagaraSystem;
+
 UCLASS()
 class ACTIONROUGELIKE_API ARougeProjectileBlackhole : public ARougeProjectile
 {
 	GENERATED_BODY()
+	
 protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UNiagaraSystem> BlackHoleEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UNiagaraComponent> LoopedBlackHoleEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Gravity")
+	TObjectPtr<URadialForceComponent> RadialForceComp;
+	
+	
+	float BlackHoleLifespan = 5.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Gravity")
+	float RadioForceRadius = 500.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Gravity")
+	float GravityStrength = -1000000.f ; //negative value to pull objects towards the center of the black hole
+	
+	FTimerHandle BlackHoleTimerHandle;
+	
+	void SelfDestroy();
 
 
 public:
 	ARougeProjectileBlackhole();
 
 	virtual void PostInitializeComponents() override;
+	
+	virtual void Tick(float DeltaTime) override;
 
 };
